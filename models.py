0d62801356623a33d75fea26f1d1b3823af5b0df
zhangwenyang <zhangwenyang@geyecloud.com>
2019-12-20 21:56:57 +0800

    抽象BaseModel 包括时间的自动加入

--- models.py
+++ models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class BaseModel(models.Model):
+    """
+    所有自定义Model需要继承的超类
+    """
+
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True)
+
+    class Meta:
+        abstract = True
